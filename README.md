3dd295212a8fffdb930403fd8241a92fbafce308
Geektree0101 <h2s1880@gmail.com>
2017-11-04 19:18:50 +0900

    start UnlimitBar

---
+++ README.md
@@ -0,0 +1,65 @@
+# UnlimitBar
+
+[![CI Status](http://img.shields.io/travis/Geektree0101/UnlimitBar.svg?style=flat)](https://travis-ci.org/Geektree0101/UnlimitBar)
+[![Version](https://img.shields.io/cocoapods/v/UnlimitBar.svg?style=flat)](http://cocoapods.org/pods/UnlimitBar)
+[![License](https://img.shields.io/cocoapods/l/UnlimitBar.svg?style=flat)](http://cocoapods.org/pods/UnlimitBar)
+[![Platform](https://img.shields.io/cocoapods/p/UnlimitBar.svg?style=flat)](http://cocoapods.org/pods/UnlimitBar)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+```swift
+    // If viewController has Scroll view
+
+        let bundle = Bundle.init(for: TestNavbar.self)
+        let nib = UINib(nibName: String(describing: TestNavbar.self), bundle: bundle)
+        let nibView = nib.instantiate(withOwner: TestNavbar.self, options: nil).first
+
+        let customNavBar = nibView as! TestNavbar // Custom NavigationBar (UIView)
+
+        self.applyNavigationBar(customNavBar,
+                                expectHeight: 44.0, // customNavBar height
+                                backButton: customNavBar.backButton, // backButton (UIButton)
+                                relatedScrollView: tableView)
+
+        // add touch target
+        customNavBar.backButton.addTarget(self, action: #selector(back), for: .touchUpInside)
+
+   // If top component has top constraint
+
+    // @IBOutlet weak var topConstraint: NSLayoutConstraint!
+
+        let bundle = Bundle.init(for: TestNavbar.self)
+        let nib = UINib(nibName: String(describing: TestNavbar.self), bundle: bundle)
+        let nibView = nib.instantiate(withOwner: TestNavbar.self, options: nil).first
+        let customNavBar = nibView as! TestNavbar
+        
+        self.applyNavigationBar(customNavBar, expectHeight: 44.0, backButton: customNavBar.backButton, subViewTopConstraint: topConstraint)
+
+        
+        customNavBar.titleLabel.text = "SingleView"
+        customNavBar.backButton.addTarget(self, action: #selector(back), for: .touchUpInside)
+        customNavBar.testButton.addTarget(self, action: #selector(hello), for: .touchUpInside)
+        
+        self.view.backgroundColor = .yellow
+```
+
+## Requirements
+
+## Installation
+
+UnlimitBar is available through [CocoaPods](http://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'UnlimitBar'
+```
+
+## Author
+
+Geektree0101, h2s1880@gmail.com
+
+## License
+
+UnlimitBar is available under the MIT license. See the LICENSE file for more info.
---
+++ UnlimitBar/Classes/UnlimitBar.swift
@@ -0,0 +1,191 @@
+import UIKit
+import Foundation
+
+fileprivate class UnlimitNavBarContainer: UIView {
+    
+    private var customNavigationBar: UIView?
+    let expectHeight: CGFloat
+    var containerIsHidden: Bool = false
+    
+    required init(statusBarColor: UIColor, expectHeight: CGFloat) {
+        self.expectHeight = expectHeight
+        super.init(frame: .zero)
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func addSubview(_ view: UIView) {
+        super.addSubview(view)
+        self.customNavigationBar = view
+    }
+    
+    func loadNavigationBar() -> UIView? {
+        return self.customNavigationBar
+    }
+}
+
+extension UIViewController {
+    
+    enum AnimateType {
+        case autohide
+        case scrollHide
+    }
+    
+    func applyNavigationBar(_ customView: UIView,
+                            expectHeight: CGFloat,
+                            backButton: UIButton?,
+                            statusBarColor: UIColor = .white,
+                            subViewTopConstraint: NSLayoutConstraint? = nil,
+                            relatedScrollView: UIScrollView? = nil) {
+        guard let navigationController = self.navigationController else { return }
+        
+        navigationController.setNavigationBarHidden(true, animated: false)
+        navigationController.navigationBar.removeFromSuperview()
+        
+        if let backButton = backButton, navigationController.viewControllers.count <= 1 {
+            backButton.removeFromSuperview()
+        }
+        
+        let statusBarHeight: CGFloat = UIApplication.shared.statusBarFrame.height
+        
+        // let hasBackButton = navigationController.viewControllers.count > 1
+        let _customNavigationBarContainerView = UnlimitNavBarContainer(statusBarColor: statusBarColor,
+                                                                       expectHeight: expectHeight)
+        _customNavigationBarContainerView.backgroundColor = statusBarColor
+        _customNavigationBarContainerView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(_customNavigationBarContainerView)
+        self.view.addConstraints(_customNavigationBarContainerViewConstraints( _customNavigationBarContainerView,
+                                                                               self.view,
+                                                                               expectHeight,
+                                                                               statusBarHeight))
+        
+        _customNavigationBarContainerView.addSubview(customView)
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        _customNavigationBarContainerView.addConstraints(self._customNavigationBarConstraint(customView,
+                                                                                             _customNavigationBarContainerView,
+                                                                                             statusBarHeight))
+        self.updateRelatedViewConstraint(contraint: subViewTopConstraint)
+        self.updateRelatedScrollViewContentInset(scrollView: relatedScrollView)
+    }
+    
+    func updateRelatedViewConstraint(contraint: NSLayoutConstraint?) {
+        guard let constraint = contraint, let containerView = self.loadUnlimitNavBarContainer() else { return }
+        constraint.constant = constraint.constant + containerView.expectHeight
+    }
+    
+    func updateRelatedScrollViewContentInset(scrollView: UIScrollView?) {
+        guard let scrollView = scrollView, let containerView = self.loadUnlimitNavBarContainer() else { return }
+        scrollView.contentInset.top = scrollView.contentInset.top + containerView.expectHeight
+    }
+    
+    
+    func transitionAnimateion(_ scrollView: UIScrollView, type: AnimateType, duration: TimeInterval = 0.5) {
+        switch type {
+        case .autohide:
+            guard let containerView = self.loadUnlimitNavBarContainer(),
+                let navBar = containerView.loadNavigationBar() else { return }
+            let expectHeight = containerView.expectHeight
+            let velocity = scrollView.panGestureRecognizer.velocity(in: self.view).y
+            
+            let animate = {
+                if velocity < 0, !containerView.containerIsHidden {
+                    containerView.containerIsHidden = true
+                    navBar.alpha = 0.0
+                    containerView.transform = CGAffineTransform(translationX: 0.0, y: -expectHeight)
+                } else if velocity > 0, containerView.containerIsHidden {
+                    navBar.alpha = 1.0
+                    containerView.containerIsHidden = false
+                    containerView.transform = .identity
+                }
+            }
+            
+            UIView.animate(withDuration: duration,
+                           delay: 0.0,
+                           options: !containerView.containerIsHidden ? .curveEaseOut: .curveEaseIn,
+                           animations: animate,
+                           completion: nil)
+        case .scrollHide:
+            // TODO: scroll hide
+            break
+        }
+    }
+    
+    private func loadUnlimitNavBarContainer() -> UnlimitNavBarContainer? {
+        for containerView in self.view.subviews {
+            guard let containerView = containerView as? UnlimitNavBarContainer else { continue }
+            return containerView
+        }
+        
+        return nil
+    }
+    
+    private func _customNavigationBarConstraint(_ customView: UIView,
+                                                _ targetView: UIView,
+                                                _ statusBarHeight: CGFloat) -> [NSLayoutConstraint] {
+        return [NSLayoutConstraint(item: customView,
+                                   attribute: .top,
+                                   relatedBy: .equal,
+                                   toItem: targetView,
+                                   attribute: .top,
+                                   multiplier: 1,
+                                   constant: statusBarHeight),
+                NSLayoutConstraint(item: customView,
+                                   attribute: .leading,
+                                   relatedBy: .equal,
+                                   toItem: targetView,
+                                   attribute: .leading,
+                                   multiplier: 1,
+                                   constant: 0.0),
+                NSLayoutConstraint(item: customView,
+                                   attribute: .trailing,
+                                   relatedBy: .equal,
+                                   toItem: targetView,
+                                   attribute: .trailing,
+                                   multiplier: 1,
+                                   constant: 0.0),
+                NSLayoutConstraint(item: customView,
+                                   attribute: .bottom,
+                                   relatedBy: .equal,
+                                   toItem: targetView,
+                                   attribute: .bottom,
+                                   multiplier: 1,
+                                   constant: 0.0)]
+    }
+    
+    private func _customNavigationBarContainerViewConstraints(_ customView: UIView,
+                                                              _ targetView: UIView,
+                                                              _ expectHeight: CGFloat,
+                                                              _ statusBarHeight: CGFloat) -> [NSLayoutConstraint] {
+        return [NSLayoutConstraint(item: customView,
+                                   attribute: .top,
+                                   relatedBy: .equal,
+                                   toItem: targetView,
+                                   attribute: .top,
+                                   multiplier: 1,
+                                   constant: 0.0),
+                NSLayoutConstraint(item: customView,
+                                   attribute: .leading,
+                                   relatedBy: .equal,
+                                   toItem: targetView,
+                                   attribute: .leading,
+                                   multiplier: 1,
+                                   constant: 0.0),
+                NSLayoutConstraint(item: customView,
+                                   attribute: .trailing,
+                                   relatedBy: .equal,
+                                   toItem: targetView,
+                                   attribute: .trailing,
+                                   multiplier: 1,
+                                   constant: 0.0),
+                NSLayoutConstraint(item: customView,
+                                   attribute: .height,
+                                   relatedBy: .equal,
+                                   toItem: nil,
+                                   attribute: .notAnAttribute,
+                                   multiplier: 1,
+                                   constant: expectHeight + statusBarHeight)]
+    }
+}
